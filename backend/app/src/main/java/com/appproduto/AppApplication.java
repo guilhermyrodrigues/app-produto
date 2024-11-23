2e4a872a5ee3a250e9412cbbed633df81074441e
Guilhermy Rodrigues <guilhermyr87@gmail.com>
2024-11-23 12:39:58 -0300

    Ajustes

--- backend/app/src/main/java/com/appproduto/AppApplication.java
+++ backend/app/src/main/java/com/appproduto/AppApplication.java
@@ -1,9 +1,12 @@
 package com.appproduto;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API Gerenciamento de Produtos", version = "1"))
 public class AppApplication {
 
 	public static void main(String[] args) {
