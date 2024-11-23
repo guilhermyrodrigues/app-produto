bf989959eac217124cf3ee9eca85a84566feb089
Guilhermy Rodrigues <guilhermyr87@gmail.com>
2024-11-23 11:55:10 -0300

    Criação do projeto

---
+++ backend/app/src/main/java/com/appproduto/AppApplication.java
@@ -0,0 +1,13 @@
+package com.appproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
